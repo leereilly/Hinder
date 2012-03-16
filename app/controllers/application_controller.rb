a0ed5b6274591c82f50b04548d3dd3d77adbc2a0
mageng <magnus.engstrom@mittmedia.se>
2012-03-16 22:38:28 +0100

    Optimize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  helper :all # include all helpers, all the time
+  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+ 
+  before_filter :detect_iphone_request
+ 
+  protected  
+  def detect_iphone_request
+    if iphone_request?
+      @using_iphone = true
+      request.format = :iphone
+    end
+  end
+ 
+  def iphone_request?
+	#request.subdomains.first == 'iphone'
+    request.env["HTTP_USER_AGENT"] && request.env["HTTP_USER_AGENT"][/(Mobile\/.+Safari)/]
+  end 
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
 	def index
-		#@canvassize = 450
-		@canvassize = 600
+    @canvassize = 600
+    respond_to do |format|
+      format.html # index.html.erb
+      format.iphone {@canvassize = 500} #index.iphone.erb
+     end
 	end
 end
