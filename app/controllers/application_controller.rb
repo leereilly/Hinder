90a5cf7305d5b315c2b236db8afaa3b676ebd579
mageng <magnus.engstrom@mittmedia.se>
2012-06-08 18:29:48 +0200

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,19 +1,4 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
- 
-  before_filter :detect_iphone_request
- 
-  protected  
-  def detect_iphone_request
-    if iphone_request?
-      @using_iphone = true
-      request.format = :iphone
-    end
-  end
- 
-  def iphone_request?
-	#request.subdomains.first == 'iphone'
-    request.env["HTTP_USER_AGENT"] && request.user_agent.include?("iPhone")
-  end 
+  protect_from_forgery # See ActionController::RequestForgeryProtection for details 
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,34 @@
 class HomeController < ApplicationController
+  	before_filter :detect_device_request
+
 	def index
-    	@canvassize = 600
-    	respond_to do |format|
-      		format.html # index.html.erb
-      		format.iphone {@canvassize = 495} #index.iphone.erb
-    	end
+		@canvassize = 600
+		@canvassize = 700 unless @using_ipad.blank?
+		respond_to do |format|
+	  		format.html # index.html.erb
+	  		format.iphone {@canvassize = 495} #index.iphone.erb
+		end
 	end
 	def show
 		redirect_to :action => 'index'
+	end
+	 
+	protected  
+	def detect_device_request
+		if iphone_request?
+		  @using_iphone = true
+		  request.format = :iphone
+		end
+		if ipad_request?
+		  @using_ipad = true
+		end
+	end
+
+	def ipad_request?
+		request.env["HTTP_USER_AGENT"] && request.user_agent.include?("iPad")
+	end
+
+	def iphone_request?
+		request.env["HTTP_USER_AGENT"] && request.user_agent.include?("iPhone")
 	end
 end
