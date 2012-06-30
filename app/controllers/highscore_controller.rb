0306f91d6eef5e28221df37787217ed4e2a39efe
mageng <magnus.engstrom@mittmedia.se>
2012-06-30 10:43:08 +0200

    bug fix

--- app/controllers/highscore_controller.rb
+++ app/controllers/highscore_controller.rb
@@ -1,38 +1,4 @@
 class HighscoreController < ApplicationController
   def index
-    if params[:moves] && params[:level]
-      @h = Highscore.new()
-      @h.moves = params[:moves].to_i
-      @h.level = params[:level].downcase
-      if @h.save
-        respond_to do |format|
-          format.js do
-            render :json => @h
-            #render @h.to_json:layout => false
-          end
-        end
-      end
-    else
-      if params[:level]
-        @highscore = Highscore.where("level = ?", params[:level].downcase).order("moves ASC").first
-        respond_to do |format|
-          format.js do
-            render :json => @highscore
-            #render @h.to_json:layout => false
-          end
-        end
-      end
-    end
-    #@highscores = Highscore.all
   end
-
-
-  def show
-    respond_to do |format|
-      format.js do
-        render :json => @h
-      end
-    end
-  end
-
 end
